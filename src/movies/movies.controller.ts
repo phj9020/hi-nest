a5cb4d060f960724228b7259e6b814ad57b64274
unknown <phj9020@hotmail.com>
2021-07-18 19:30:48 +0900

    Search added, Patch returns new object with movieId and updatedData

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
     @Get() 
     getAll(){
         return "This will return all movies"
+    }
+    @Get("search")
+    search(@Query("year") searchYear: string){
+        return `we are searching for a movie with a title : ${searchYear}`
     }
     // get single movie with id
     @Get("/:id") 
@@ -13,16 +17,21 @@ export class MoviesController {
     }
     // create movie
     @Post()
-    create(){
-        return "This will create a movie";
+    create(@Body() movieData){
+        return movieData;
     }
+
     @Delete("/:id")
     remove(@Param("id") movieId: string){
         return `this will delete a movie with the id: ${movieId}`;
     }
     // Patch: partial update | Put : whole resource update
     @Patch("/:id")
-    update(@Param("id") movieId: string){
-        return `this will update a movie with the id: ${movieId}`;
+    update(@Param("id") movieId: string, @Body() updateData){
+        return {
+            updatedMovieId : movieId,
+            ...updateData,
+        }
     }
+    
 }
