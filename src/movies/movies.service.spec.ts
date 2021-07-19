a3cad4644e6fcfb05a4a0c4029eb2d4f6f469069
unknown <phj9020@hotmail.com>
2021-07-19 17:52:16 +0900

    create & delete unit test code added

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -50,5 +50,42 @@ describe('MoviesService', () => {
 
   });
 
+  describe("delete", () => {
+    it("should delete movie", () => {
+      service.create({
+        title: "test Movie",
+        year: 2021,
+        genres: ["horror", "comedy"]
+      });
+      const deforeDelete = service.getAll().length;
+      service.remove(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toEqual(deforeDelete - 1);
+    });
+
+    it("should return 404", ()=> {
+      try {
+        service.remove(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+
+  });
+
+  describe("create", ()=> {
+    it("should create a new movie", ()=> {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: "test Movie",
+        year: 2021,
+        genres: ["horror", "comedy"]
+      });
+      const afterCreate = service.getAll().length;
+      console.log(beforeCreate, afterCreate);
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+      
+    });
+  });
 
 });
